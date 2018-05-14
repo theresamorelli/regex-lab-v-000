eeef0de48070e270ffb39c5c08dc4167c58fd1cd
Theresa Morelli <theresa.l.morelli@gmail.com>
2018-05-14 21:15:38 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  !!phone.match(/\(?\d{3}\)?\s?\-?\d{3}-?\s?\d{4}/)
+  !!phone.match(/^\d{3}[ -]?\d{3}[ -]?\d{4}|\(\d{3}\)\s?\d{3}[- ]?\d{4}/)
 end
-
-starts_with_a_vowel?("apple")
